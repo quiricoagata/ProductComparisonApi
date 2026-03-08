b9bb3c59889e03d73c909b2c6401367c386be748
Ágata Quirico <quiricoagata@gmail.com>
2026-03-08 15:03:21 -0300

    refactor: agrego model controller para probar casos de prueba en hackerrank

---
+++ ProductComparisonApi.API/Controllers/ModelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+using ProductComparisonApi.API.Validator;
+using ProductComparisonApi.Application.Interfaces;
+
+namespace ProductComparisonApi.Controllers
+{
+    [ExcludeFromCodeCoverage]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("model")]
+    public class ModelController : ProductsController
+    {
+        public ModelController(
+            IProductService productService,
+            ProductValidator validator,
+            ILogger<ProductsController> logger)
+            : base(productService, validator, logger)
+        {
+        }
+    }
+}
